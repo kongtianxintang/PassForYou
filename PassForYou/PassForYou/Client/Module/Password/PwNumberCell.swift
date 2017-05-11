f79eba74a8d57f1b2b24b2c539c2414994d03e9d
kongtianxintang <kongtianxintang@163.com>
2017-05-11 14:04:45 +0800

    add:touchid 图片

---
+++ PassForYou/PassForYou/Client/Module/Password/PwNumberCell.swift
@@ -0,0 +1,18 @@
+//
+//  PwNumberCell.swift
+//  PassForYou
+//
+//  Created by mtkj on 2017/5/10.
+//  Copyright © 2017年 mtkj. All rights reserved.
+//
+
+import UIKit
+
+class PwNumberCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
