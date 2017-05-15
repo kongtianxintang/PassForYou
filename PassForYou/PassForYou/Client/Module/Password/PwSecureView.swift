9c34260c8cabe3ce5e0bb9d9c439cb789b630a3e
kongtianxintang <kongtianxintang@163.com>
2017-05-15 10:07:19 +0800

    add:删除按钮

---
+++ PassForYou/PassForYou/Client/Module/Password/PwSecureView.swift
@@ -0,0 +1,21 @@
+//
+//  PwSecureView.swift
+//  PassForYou
+//
+//  Created by mtkj on 2017/5/12.
+//  Copyright © 2017年 mtkj. All rights reserved.
+//
+
+import UIKit
+
+class PwSecureView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
